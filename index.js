316cf30c799cc765550b767c8d19c522572aaf6f
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-09-03 13:48:03 +0700

    chore: setup eslint and prettier

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const dotenv = require('dotenv');
+
 const app = express();
 
 dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:8080'
-}
+  origin: 'http://localhost:8080'
+};
 
 app.use(cors(corsOptions));
 
@@ -18,18 +19,18 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Mabolista simple route'
-    })
-})
+  res.json({
+    message: 'Mabolista simple route'
+  });
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-})
+  console.log(`Server is running on port ${PORT}.`);
+});
