9de40fe0c8e01f18a21a3c77b14527ec1d2797a0
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-09-07 13:04:56 +0700

    chore: change user route naming import

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const userRouter = require('./modules/user/user.router');
 
 const app = express();
 
@@ -30,6 +31,8 @@ app.get('/', (req, res) => {
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
