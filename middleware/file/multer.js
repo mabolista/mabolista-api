5792513abffeaebdf6ba7e5b7cdb6d5a2bbb93b4
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-09-10 10:58:48 +0700

    feat: upload image to cloudinary

---
+++ middleware/file/multer.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = { upload };
