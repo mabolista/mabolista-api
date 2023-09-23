881f940aa913173dfd473036329db8ced951f882
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-09-23 19:43:53 +0700

    chore: add checking verify jwt

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const { findUserById } = require('../modules/user/user.service');
+const { findEmployeeById } = require('../modules/employee/employee.service');
 
 dotenv.config();
 
@@ -12,10 +13,15 @@ const generateToken = (data) => {
   return token;
 };
 
-const verifyToken = (token) => {
+const verifyToken = (token, isUserAdmin) => {
   if (!token) return null;
 
   const { id } = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  if (isUserAdmin) {
+    const user = findEmployeeById(id);
+    return user;
+  }
 
   const user = findUserById(id);
 
