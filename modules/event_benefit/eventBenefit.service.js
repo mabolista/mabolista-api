4e5b1cdc2857abb0a76a81aae403a07ab11a4601
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-10-13 06:53:57 +0700

    chore: add eventBenefit service

--- modules/event_benefit/eventBenefit.service.js
+++ modules/event_benefit/eventBenefit.service.js
@@ -1,37 +1,24 @@
-const models = require("../../core/database/models");
+const models = require('../../core/database/models');
 
-const createEmployeeBenefit = async ({
-                               title,
-                               image_url,
-                               image_public_id,
-                               description,
-                               location,
-                               gmaps_url,
-                               notes,
-                               player_price,
-                               keeper_price,
-                               event_date,
-                               start_time,
-                               end_time
-                           }) => {
-    const event = await models.Event.create({
-        title,
-        image_url,
-        image_public_id,
-        description,
-        location,
-        gmaps_url,
-        notes,
-        player_price,
-        keeper_price,
-        event_date,
-        start_time,
-        end_time
-    });
+const createEventBenefit = async (data, transaction) => {
+  const eventBenefit = await models.EventBenefit.bulkCreate(data, {
+    transaction,
+    returning: true
+  });
 
-    return event.toJSON();
+  return eventBenefit;
 };
 
-module,exports = {
+const deleteEventBenefit = async (eventId, transaction) => {
+  eventId = parseInt(eventId, 10);
 
-}
+  await models.EventBenefit.destroy({
+    where: { eventId },
+    transaction
+  });
+};
+
+module.exports = {
+  createEventBenefit,
+  deleteEventBenefit
+};
