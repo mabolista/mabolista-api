c85028eb58f68426065b0bb9e7099cb585e10c17
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2023-12-10 17:46:51 +0700

    chore: add cloudinary media folder path

--- shared-v1/constants/index.js
+++ shared-v1/constants/index.js
@@ -1,7 +1,9 @@
 const errorCode = require('./errorCode');
 const errorStatusCode = require('./errorStatusCode');
+const cloudinaryMediaFolderPath = require('./cloudinaryMedia');
 
 module.exports = {
+  cloudinaryMediaFolderPath,
   errorCode,
   errorStatusCode
 };
