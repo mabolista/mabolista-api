1ecc86c5b8903fb257881ede51f19e999e60b68a
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2024-03-06 07:48:48 +0700

    fix(cors): handle cors manually

--- api/index.js
+++ api/index.js
@@ -9,13 +9,26 @@ const apiDocumentation = require('../apidocs.json');
 
 const app = express();
 
-const corsOptions = {
-  origin: '*',
-  methods: '*',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  next();
+});
+
+// const corsOptions = {
+//   origin: '*',
+//   methods: '*',
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// };
+// app.use(cors(corsOptions));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
 
