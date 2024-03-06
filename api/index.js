18a5835ef587db23edf5b372214151e24504dc99
Ilham Alamsyah <ilhamalamsyah510@gmail.com>
2024-03-06 08:26:10 +0700

    fix(cors): try cors allow origin to locahost:3000

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const apiDocumentation = require('../apidocs.json');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET,OPTIONS,PATCH,DELETE,POST,PUT'
